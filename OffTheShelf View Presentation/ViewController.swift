114c94a95ce42136f685f68dfe989c1ad8f3a227
David Lee <itxawjteeb@gmail.com>
2015-06-02 20:07:41 -0700

    Testing out the alert view controller on iOS

--- OffTheShelf View Presentation/ViewController.swift
+++ OffTheShelf View Presentation/ViewController.swift
@@ -15,11 +15,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func experiment(){
+        
+        // <---------Image Picker Controller---------->
+//        let controller = UIImagePickerController()
+//        self.presentViewController(controller, animated: true, completion: nil)
+//        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.Default) { action in self.dismissViewControllerAnimated(true, completion: nil)
+//        }
+        
+        
+        // <---------Activity View Controller---------->
+//        let image = UIImage()
+//        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+//        self.presentViewController(controller, animated: true, completion: nil)
+//        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.Default) { action in self.dismissViewControllerAnimated(true, completion: nil)
+//        }
+        
+        
+        // <---------Alert View Controller---------->
+        let controller = UIAlertController()
+        controller.title = "Test alert"
+        controller.message = "This is a test"
+        
+        let okAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.Default) { action in self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        controller.addAction(okAction)
+        self.presentViewController(controller, animated: true, completion: nil)
+        
     }
-
-
+    
 }
 
